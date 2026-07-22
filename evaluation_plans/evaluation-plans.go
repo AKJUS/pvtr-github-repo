2376ddb069d17510605a9e19b81e082028795e9d
Eddie Knight <knight@linux.com>
2026-07-21 22:12:06 -0500

    feat: pass GV-01 on observed governance/ownership files (#383)
    Signed-off-by: Eddie Knight <knight@linux.com>

--- evaluation_plans/evaluation-plans.go
+++ evaluation_plans/evaluation-plans.go
@@ -105,7 +105,6 @@ var (
 			docs.HasDependencyManagementPolicy,
 		},
 		"OSPS-GV-01.01": {
-			reusable_steps.HasSecurityInsightsFile,
 			reusable_steps.IsActive,
 			governance.CoreTeamIsListed,
 			governance.ProjectAdminsListed,
